cceaf33c78964bb1e79939ea19b39daf7d030679
Janvi <janvisiddhapara98@gmail.com>
2022-12-31 11:17:31 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+
+public class sum
+{
+    public static void main(String[] args)
+    {
+        int a=10 ;
+        int b=15;
+        int c =a+b;
+        System.out.println(c);
+         
+
+    }
+}
